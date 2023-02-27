79087aaf46bee9437a124e82991c5d09eb2c1d7f
Aiken Wong <aikenwong55@gmail.com>
2023-02-28 00:51:32 +0800

    test

--- myshell.c
+++ myshell.c
@@ -321,7 +321,7 @@ static void command_exec(char* program, char** command, int num_tokens) {
         
             int output_file = open("./a.txt", O_WRONLY | O_CREAT | O_TRUNC);
 
-            dup2(output_file, STDOUT_FILENO);
+            // dup2(output_file, STDOUT_FILENO);
             close(output_file);
         }
 
