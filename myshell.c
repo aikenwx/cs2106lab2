21312f2907a3c7539e795a4de3067a22bef3fa5a
Aiken Wong <aikenwong55@gmail.com>
2023-03-01 04:18:38 +0800

    test

--- myshell.c
+++ myshell.c
@@ -124,7 +124,7 @@ static void handle_child_process_exited_or_stopped() {
     int w_status;
 
     printf("handle_child_process_exited_or_stopped called");
-    child_pid = wait(&w_status);
+    child_pid = waitpid(-1, &w_status, WNOHANG);
 
 
     if (child_pid == -1) {
