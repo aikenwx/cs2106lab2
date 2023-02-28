a65728284992deeaa9ae30643a335f64644de243
Aiken Wong <aikenwong55@gmail.com>
2023-03-01 02:34:12 +0800

    test

--- myshell.c
+++ myshell.c
@@ -110,10 +110,8 @@ static void signal_handler(int signo) {
         exit(2);
     } else if (signo == SIGINT && child_pid != -1) {
         printf("[%d] interrupted\n", child_pid);
-        proc_update_status(child_pid, TERMINATING, 2);
     } else if (signo == SIGTSTP && child_pid != -1) {
         printf("[%d] stopped\n", child_pid);
-        proc_update_status(child_pid, STOPPED, 2);
     }
     
 // Use the signo to identy ctrl-Z or ctrl-C and print “[PID] stopped or print “[PID] interrupted accordingly.
