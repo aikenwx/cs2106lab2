e1dcf049744a098cf6ffb26c9269771888f3e869
Aiken Wong <aikenwong55@gmail.com>
2023-03-01 04:25:38 +0800

    test

--- myshell.c
+++ myshell.c
@@ -123,27 +123,33 @@ static void signal_handler(int signo) {
     // Update the status of the process in the PCB table
 }
 
-static void handle_child_process_exited_or_stopped() {
-    // pid_t child_pid;
-    // int w_status;
-    // child_pid = wait(&w_status);
+static void handle_child_process_exited_or_stopped(int signo) {
+    pid_t child_pid;
+    int w_status;
+
+    if (signo == SIGCHLD) {
+        printf("signal handler called");
+        return;
+    }
+
+    child_pid = wait(&w_status);
 
 
-    // if (child_pid == -1) {
-    //     // PID of -1 means no current child process
-    //     return;
-    // }
-    // // Child exited under control
-    // if (WIFEXITED(w_status)) {
-    //     printf("[%d] exited under control\n", child_pid);
+    if (child_pid == -1) {
+        // PID of -1 means no current child process
+        return;
+    }
+    // Child exited under control
+    if (WIFEXITED(w_status)) {
+        printf("[%d] exited under control\n", child_pid);
 
-    //     proc_update_status(child_pid, EXITED, WEXITSTATUS(w_status));
-    // }
-    // // Child did not exit normally
-    // if (WIFSIGNALED(w_status)) {
-    //     printf("[%d] exited abnormally\n", child_pid);
-    //     proc_update_status(child_pid, EXITED, WTERMSIG(w_status));
-    // }
+        proc_update_status(child_pid, EXITED, WEXITSTATUS(w_status));
+    }
+    // Child did not exit normally
+    if (WIFSIGNALED(w_status)) {
+        printf("[%d] exited abnormally\n", child_pid);
+        proc_update_status(child_pid, EXITED, WTERMSIG(w_status));
+    }
 }
 
 /*******************************************************************************
